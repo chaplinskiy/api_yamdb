566854e276a6c57f896aa7d0666dcb61f7a92490
Ivan Chaplinskiy <chaplinskiy@ya.ru>
2021-09-15 01:23:01 +0300

    fucking git

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -7,3 +7,7 @@ def year_validator(value):
     if value < 1 or value > timezone.now().year:
         raise ValidationError('Проверьте год!')
     return value
+
+
+def let_me_name_you_as_i_want():
+    pass
